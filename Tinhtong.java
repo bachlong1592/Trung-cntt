de44fe2077adb79bb4e7385c056e66781768c1d5
Trương Văn Trung <113073858+bachlong1592@users.noreply.github.com>
2022-10-05 23:43:12 +0700

    Add files via upload

---
+++ Tinhtong.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Tinhtong {
+    public static void main(String[] args) {
+        int n, sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập n: ");
+        n = sc.nextInt();
+        for(int i = 1; i <= n; ++i)
+        {
+            sum += i;
+        }
+        System.out.println("Tổng các số tự nhiên từ 1 đến "+ n +" là: "+sum);
+    }
+}
+
